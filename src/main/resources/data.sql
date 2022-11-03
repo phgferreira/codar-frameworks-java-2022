7460468a6cb2d77f0e0ccbdad257d1a1c497219f
Eduardo Branquinho <eduardo.branquinho@bluesoft.com.br>
2022-11-03 12:55:26 -0300

    adicionando schema e data das tabelas base

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into carro values ('ABC-1234', 'Hyundai', 'HB20', 'Prata', 2018, 43400, 95.50)
+insert into carro values ('ABC-1234', 'Hyundai', 'HB20', 'Prata', 2018, 43400, 95.50);
+insert into cliente (nome, cpf, email, celular) values ('Luis Paulo Supimpa', 39371890017, 'luis_paulo@gmail.com', 951789834);
+insert into endereco (rua, numero, complemento, bairro, cidade, estado, cliente_key) values ('Rua Borboletas', 123, 'Ao lado da padaria', 'Centro', 'Santo André', 'SP', 1);
+insert into vendedor (nome, cpf, data_admissao) values ('Rodrigo Limeira Cachias', 34378748819, '2015-04-18');
+insert into conta_corrente (banco, agencia, conta_corrente, vendedor_key) values ('SANTANDER', 133, 2343, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,39 @@ CREATE TABLE carro (
    quilometragem INT NOT NULL,
    diaria FLOAT NOT NULL
 );
+
+CREATE TABLE cliente (
+                         cliente_key INT PRIMARY KEY AUTO_INCREMENT,
+                         nome VARCHAR(100) NOT NULL,
+                         cpf BIGINT NOT NULL,
+                         email VARCHAR(50) NOT NULL,
+                         celular BIGINT NOT NULL
+);
+
+CREATE TABLE endereco (
+                          endereco_key INT PRIMARY KEY AUTO_INCREMENT,
+                          rua VARCHAR(50) NOT NULL,
+                          numero INT NOT NULL,
+                          complemento VARCHAR(50),
+                          bairro VARCHAR(50) NOT NULL,
+                          cidade VARCHAR(50) NOT NULL,
+                          estado VARCHAR(2) NOT NULL,
+                          cliente_key INT NOT NULL,
+                          foreign key (cliente_key) references cliente(cliente_key)
+);
+
+CREATE TABLE vendedor (
+  vendedor_key INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  cpf BIGINT NOT NULL,
+  data_admissao DATE NOT NULL
+);
+
+CREATE TABLE conta_corrente (
+  conta_corrente_key INT PRIMARY KEY AUTO_INCREMENT,
+  banco VARCHAR(50) not null,
+  agencia INT NOT NULL,
+  conta_corrente INT NOT NULL,
+  vendedor_key INT NOT NULL,
+  foreign key (vendedor_key) references vendedor(vendedor_key)
+);
